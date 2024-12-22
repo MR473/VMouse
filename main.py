eae198c8382a16ae8264d041b8317518bd781c16
MR473 <mohnish.ramachandran@gmail.com>
2024-12-22 16:47:15 +0530

    Run this for Virtual Mouse

---
+++ main.py
@@ -0,0 +1,51 @@
+import cv2
+import mediapipe as mp
+import util
+import functionality
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(
+    static_image_mode= False,
+    model_complexity= 1,
+    min_detection_confidence= 0.7, 
+    min_tracking_confidence= 0.7,
+    max_num_hands= 1)
+
+def main():
+    cap = cv2.VideoCapture(0)
+    draw = mp.solutions.drawing_utils
+    try:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            frame = cv2.flip(frame, 1)
+            frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            processed = hands.process(frameRGB)
+
+            landmarks_list = []
+
+            if processed.multi_hand_landmarks:
+                hand_landmarks = processed.multi_hand_landmarks[0]
+                draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
+                
+                for lm in hand_landmarks.landmark:
+                    landmarks_list.append((lm.x, lm.y))
+
+                #print(landmarks_list)
+            
+            # IDENTIFIES GESTURES
+            functionality.detect_gesture(frame, landmarks_list, processed)
+
+            cv2.imshow("FRAME", frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+    finally:
+        cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
